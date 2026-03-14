c3a00be88b32dc77e315d7eef60d38ced1aa0554
akhilreddy20 <akhilreddy8688@gmail.com>
2026-03-14 17:03:53 +0530

    commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Hero        from '../components/Hero.jsx';
 import Marquee     from '../components/Marquee.jsx';
 import Programs    from '../components/Programs.jsx';
 import WhyStudy    from '../components/Whystudy.jsx';
-import HallOfFame  from '../components/HallOffame.jsx';
+import HallOfFame  from '../components/Halloffame.jsx';
 import GlobalTieUps from '../components/GlobaltieUps.jsx';
 import Placements  from '../components/Placements.jsx';
 
